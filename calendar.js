c5334089d9692b40fb5b9dd861079cd8124ea84b
Matt Kulukundis <matt.fowles@gmail.com>
2024-06-13 10:56:11 -0400

    Clean up the code a little

--- calendar.js
+++ calendar.js
@@ -1,6 +1,4 @@
-const calendarId =
-  'd34a7730554333eb96c45c5cc66ef691a120630af8507e670f5a364abfa5e033@group.calendar.google.com';
-// const calendarId = 'primary';
+const calendarId = 'primary';
 
 const skeletorColors = [
   CalendarApp.EventColor.YELLOW,
@@ -10,68 +8,14 @@ const skeletorColors = [
 
 function updateAllSkeletons() {
   const skeletons = buildSkeletonMap();
-  recolorSkeletons(skeletons);
+  colorSkeletons(skeletons);
   rescheduleSkeletons(skeletons);
 }
 
-function rescheduleSkeletons(skeletons) {
-  for (const key in skeletons) {
-    const events = skeletons[key];
-    console.log("Evaluating group '%s'...", key);
-    rescheduleEvents(events);
-  }
-}
-
-function rescheduleEvents(events) {
-  var offset_event = null;
-  for (const e of events) {
-    if (e.metadata.start_time !== e.getStartTime().toISOString()) {
-      if (offset_event !== null) {
-        console.log("Skeletor group '%s' has multiple moved events!", e.metadata.key);
-        recolorEvents(events, CalendarApp.EventColor.RED);
-        return;
-      }
-      offset_event = e;
-    }
-  }
-  if (offset_event === null) return;
-
-
-  const offset = computeOffset(offset_event);
-  console.log("Moving group '%s' by %d minutes...", offset_event.metadata.key, offset / (60 * 1000));
-  for (const e of events) {
-    if (e === offset_event) continue;
-    const ts = e.getStartTime();
-    const te = e.getEndTime();
-    ts.setMilliseconds(offset);
-    te.setMilliseconds(offset);
-    e.setTime(ts, te);
-  }
-
-  for (const e of events) {
-    e.metadata = buildTagForEvent(e, e.metadata.key);
-  }
-}
-
-function computeOffset(e) {
-  const o = new Date(e.metadata.start_time);
-  const n = e.getStartTime();
-  return n - o;
-}
-
-function recolorSkeletons(skeletons) {
-  var colorIndex = 0;
-  for (const key in skeletons) {
-    const events = skeletons[key];
-    const color = skeletorColors[colorIndex++ % skeletorColors.length];
-    recolorEvents(events, color);
-  }
-}
-
-function recolorEvents(events, color) {
-  for (const e of events) {
-    e.setColor(color);
-  }
+function getKeyForEvent(e) {
+  let match = (/^skeletor:\s*(.*)$/m).exec(e.getDescription());
+  if (!match) return null;
+  return match[1];
 }
 
 function buildSkeletonMap() {
@@ -86,27 +30,91 @@ function buildSkeletonMap() {
   for (const e of events) {
     const key = getKeyForEvent(e);
     if (!key) continue;
-    
 
-    var t = e.getTag("skeletor");
-    if (t) {
-      t = JSON.parse(t);
-      if (t.key !== key) t = null;
-    }
-
-    if (!t) t = buildTagForEvent(e, key);
-    e.metadata = t;
+    e.metadata = getTagForEvent(e, key);
     (eventsByKey[key] = eventsByKey[key] || []).push(e);
   }
 
   return eventsByKey;
 }
 
-function getKeyForEvent(e) {
-  var desc = e.getDescription();
-  let match = (/^skeletor:\s*(.*)$/m).exec(desc);
-  if (!match) return null;
-  return match[1];
+function rescheduleSkeletons(skeletons) {
+  for (const key in skeletons) {
+    const events = skeletons[key];
+    console.log("Evaluating group '%s'...", key);
+    rescheduleSkeleton(events);
+  }
+}
+
+function rescheduleSkeleton(events) {
+  var offset_event = null;
+  for (const e of events) {
+    if (e.metadata.start_time !== e.getStartTime().toISOString()) {
+      if (offset_event !== null) {
+        console.error("Skeletor group '%s' has multiple moved events!",
+                      e.metadata.key);
+        colorEvents(events, CalendarApp.EventColor.RED);
+        return;
+      }
+      offset_event = e;
+    }
+  }
+  if (offset_event === null) return;
+
+  const offset_ms = computeOffsetInMilliseconds(offset_event);
+  console.log("Moving group '%s' by %d minutes...", offset_event.metadata.key,
+              millisecondsToMinutes(offset_ms));
+  for (const e of events) {
+    if (e === offset_event) continue;
+
+    e.setTime(offsetTime(e.getStartTime(), offset_ms),
+              offsetTime(e.getEndTime(), offset_ms));
+  }
+
+  for (const e of events) {
+    e.metadata = buildTagForEvent(e, e.metadata.key);
+  }
+}
+
+function computeOffsetInMilliseconds(e) {
+  const o = new Date(e.metadata.start_time);
+  const n = e.getStartTime();
+  return n - o;
+}
+
+function millisecondsToMinutes(ms) {
+  return ms / (60 * 1000);
+}
+
+function offsetTime(t, offset_ms) {
+  t.setMilliseconds(t.getMilliseconds() + offset_ms);
+  return t;
+}
+
+function colorSkeletons(skeletons) {
+  var colorIndex = 0;
+  for (const key in skeletons) {
+    const events = skeletons[key];
+    const color = skeletorColors[colorIndex++ % skeletorColors.length];
+    colorEvents(events, color);
+  }
+}
+
+function colorEvents(events, color) {
+  for (const e of events) {
+    e.setColor(color);
+  }
+}
+
+function getTagForEvent(e, key) {
+  var t = e.getTag("skeletor");
+  if (t) {
+    t = JSON.parse(t);
+    if (t.key !== key) t = null;
+  }
+
+  if (!t) t = buildTagForEvent(e, key);
+  return t;
 }
 
 function buildTagForEvent(e, key) {
